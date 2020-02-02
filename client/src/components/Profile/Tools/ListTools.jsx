be4ece4016d65a5f8b03d0665ce04ad54353000c
EdReactDeveloper <edinbox30@gmail.com>
2020-02-02 15:12:12 +0300

    hide insert button

--- client/src/components/Profile/Tools/ListTools.jsx
+++ client/src/components/Profile/Tools/ListTools.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import Button from '../../misc/Elements/Button';
-import style from './tools.module.scss'; 
+import style from './tools.module.scss';
 
 const ListTools = (props) => {
   const {
     data: { page, selectedItems },
     methods: { moveItemsHandler,
-    } 
+    }
   } = props
   const numberOfItems = selectedItems.length
   const isInside = selectedItems.some(item => item.parentId === page._id)
   return (
     <div className={style.listTools}>
       <Button type="add" > add item</Button>
-      <Button type="move" onClick={() => moveItemsHandler({ id: page._id, items: selectedItems })}>
-        {numberOfItems && !isInside ? 
-        <>insert ({numberOfItems})</>
-        : 
-        <>no items selected</>
+      {!isInside && numberOfItems ? <Button type="move" onClick={() => moveItemsHandler({ id: page._id, items: selectedItems })}>
+        {numberOfItems ?
+          <>insert ({numberOfItems})</>
+          :
+          <>no items selected</>
         }
-        </Button>
+      </Button> : <></>}
     </div>
   );
 };
